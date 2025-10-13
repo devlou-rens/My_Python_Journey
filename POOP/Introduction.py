ae4bd5792339ed0da0ef48ed25fd85842c521901
Lourens Gacias <lourensgacias00@gmail.com>
2025-10-13 08:49:53 +0800

    Py_OOP

---
+++ POOP/Introduction.py
@@ -0,0 +1,14 @@
+class Intro:
+    def __init__(self, full_name, age): #constructor
+        self.full_name = full_name
+        self.age = age
+    
+    def introduce(self): #method
+        print(f"Hi I'm {self.full_name}, and I'm {self.age} years old.")
+
+introObj = Intro("devlou-rens", 21) #object
+
+#display
+print(introObj.full_name) #Output: devlou-rens
+print(introObj.age)       #Output: 21
+introObj.introduce()      #Output: Hi I'm devlou-rens, and I'm 21 years old.
